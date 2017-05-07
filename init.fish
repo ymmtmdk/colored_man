36c0b44d3eeafcafb36a93b54c140f267d8f62c9
ymmtmdk <ymmtmdk@gmail.com>
2017-05-07 18:05:57 +0900

    Export EVNVAR. https://unix.stackexchange.com/questions/87261/getting-unexpected-colorized-output-on-several-commands

---
+++ init.fish
@@ -0,0 +1,8 @@
+set -x LESS_TERMCAP_mb (set_color blue)
+set -x LESS_TERMCAP_md (set_color -o blue)
+set -x LESS_TERMCAP_me (set_color normal)
+set -x LESS_TERMCAP_so (set_color yellow)
+set -x LESS_TERMCAP_se (set_color normal)
+set -x LESS_TERMCAP_us (set_color magenta)
+set -x LESS_TERMCAP_ue (set_color normal)
+
